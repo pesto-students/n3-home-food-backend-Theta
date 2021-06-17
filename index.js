d6d2f2db665a972a85cd499eb108aa48f43949d7
sahil thakare <sahilthakare1998@gmail.com>
2021-06-17 10:08:53 +0530

    admin apis completed

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv/config");
+const authJwt = require("./config/jwt");
+
 const errorHandler = require('./config/error-handler');
 
 app.use(cors());
@@ -12,16 +14,23 @@ app.options("*", cors());
 //middleware
 app.use(express.json());
 app.use(morgan("tiny"));
-app.use(errorHandler)
+//app.use(authJwt());
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
+app.use(errorHandler);
 
 const api = process.env.API_URL;
 
 //Routes
 
 const adminRoutes = require("./routes/admin");
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const sellerRoutes = require("./routes/sellers");
 
 app.use(`/${api}/admin`,adminRoutes);
-
+app.use(`/${api}/categories`, categoriesRoutes);
+app.use(`/${api}/products`, productsRoutes);
+app.use(`/${api}/sellers`, sellerRoutes);
 
 
 //Database
---
+++ models/seller.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+
+const sellerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    passwordHash: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    image:{
+        type:String,
+        default:''
+    },
+    customerType: {
+        type: String,
+        default:'Seller'
+    },
+    adress: {
+        type: String,
+        default: ''
+    },
+    idProof:{
+        type:String,
+        required: true,
+    },
+    products:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Product'
+    }],
+    productCategories:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Category'
+    }],
+    requestedProducts:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'Product'
+    }],
+    status:{
+        type:String,
+        default:'Pending'
+    },
+    rejection_reason:{
+        type: String
+    }
+
+
+});
+
+sellerSchema.virtual('id').get(function () {
+    return this._id.toHexString();
+});
+
+sellerSchema.set('toJSON', {
+    virtuals: true,
+});
+
+exports.Seller = mongoose.model('seller', sellerSchema);
+
+//exports.sellerSchema = sellerSchema;
