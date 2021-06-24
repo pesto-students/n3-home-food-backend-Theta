634c7ea7d4f606d1596464ff258da5a4b0cb3df9
sahil thakare <sahilthakare1998@gmail.com>
2021-06-25 01:41:26 +0530

    resolvings empty items of cart

--- routes/cart.js
+++ routes/cart.js
@@ -23,7 +23,7 @@ router.get(`/:id`, async (req, res) => {
   if(!user) res.status(500).json({success:false})
 
   const cart = await Cart.findById(user.cart).populate({path:'items',populate:'productId'})
-  if(!cart) res.status(500).json({success:false})
+  if(!cart) res.status(500).json({ items:[] })
 
 
   res.status(200).send(cart);
