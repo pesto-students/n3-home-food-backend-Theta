cc339f8d67dab9796b9406061cf37cfb5c9e113f
sahil thakare <sahilthakare1998@gmail.com>
2021-07-08 20:55:36 +0530

    resolving the images issue

--- routes/products.js
+++ routes/products.js
@@ -47,9 +47,8 @@ router.post(`/`, uploadOptions.single("image"), async (req, res) => {
 
     const category = await Category.findById(item);
     if (!category) return res.status(400).send("Invalid Category");
-
     const file = req.file;
-    
+
     if (!file) return res.status(400).send("No image in the request");
 
     const fileName = file.filename;
